a5b1563cf4d735f207a884200462b615aead9780
Matheus Alves <theuves@gmail.com>
2021-08-30 20:44:07 -0400

    add subscription to SNS

--- sns.tf
+++ sns.tf
@@ -2,3 +2,10 @@
 resource "aws_sns_topic" "main" {
   name = var.name
 }
+
+# Call AWS Lambda when publish messages
+resource "aws_sns_topic_subscription" "main" {
+  topic_arn = aws_sns_topic.main.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.main.arn
+}

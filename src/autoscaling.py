46b2ee65ee950e91798151b7774960af35f51565
Matheus Alves <theuves@gmail.com>
2021-08-31 23:09:05 -0400

    create autoscaling.py and add handler

---
+++ src/autoscaling.py
@@ -0,0 +1,150 @@
+import boto3
+import uuid
+
+client = boto3.client('docdb')
+
+class DocumentDB:
+  """
+  Add or remove read replicas on an Amazon DocumentDB cluster.
+
+  Attributes
+  ----------
+  db_cluster_id : str
+    DocumentDB cluster identifier.
+  min_capacity : int
+    The minimum capacity (default is 0).
+  max_capacity : int
+    The maximum capacity (default is 15).
+
+  Methods
+  -------
+  is_modifying()
+    Check if some instance in the cluster is not "available".
+  get_replicas_count()
+    Get how many replicas there are in the cluster.
+  get_primary_instance_class()
+    Get the class of the primary instance (writer).
+  add_replica()
+    Add one read replica.
+  remove_replica()
+    Remove one read replica.
+  """
+  def __init__(self, db_cluster_id, min_capacity=0, max_capacity=15):
+    """
+    Parameters
+    ----------
+    db_cluster_id : str
+      DocumentDB cluster identifier.
+    min_capacity : int
+      The minimum capacity (default is 0).
+    max_capacity : int
+      The maximum capacity (default is 15).
+    """
+    self.db_cluster_id = db_cluster_id
+    self.min_capacity = min_capacity
+    self.max_capacity = max_capacity
+
+    # Needed to know what is the 'primary instance'
+    self.db_clusters = client.describe_db_clusters(DBClusterIdentifier=db_cluster_id)
+
+    # Needed to know the instance class
+    self.db_instances = client.describe_db_instances(
+      Filters=[{
+        'Name': 'db-cluster-id',
+        'Values': [ db_cluster_id ]
+      }]
+    )
+
+  def is_modifying(self):
+    """
+    Check if some instance in the cluster is not "available".
+
+    Returns
+    -------
+    bool
+      Returns False if the status all instance is "available" else returns True.
+    """
+    db_instances = self.db_instances.get('DBInstances')
+    for db_instance in db_instances:
+      if db_instance.get('DBInstanceStatus') != 'available':
+        return True
+
+    return False
+
+  def get_replicas_count(self):
+    """
+    Get how many replicas there are in the cluster.
+
+    Returns
+    -------
+    int
+      Replicas count.
+    """
+    db_instances = self.db_instances.get('DBInstances')
+    return len(db_instances[1:])
+  
+  def get_primary_instance_class(self):
+    """
+    Get the class of the primary instance (writer).
+    
+    Returns
+    -------
+    str
+      Class of the primary instance, if not found will returns "db.r5.large".
+    """
+    db_cluster_members = self.db_clusters.get('DBClusters')[0].get('DBClusterMembers')
+    db_instances = self.db_instances.get('DBInstances')
+
+    for cluster_member in db_cluster_members:
+      is_cluster_writer = cluster_member.get('IsClusterWriter')
+      if is_cluster_writer:
+        for db_instance in db_instances:
+          if cluster_member.get('DBInstanceIdentifier') == db_instance.get('DBInstanceIdentifier'):
+            return db_instance.get('DBInstanceClass')
+    
+    # In theory this block never can be executed
+    return 'db.r5.large'
+
+  def add_replica(self):
+    """
+    Add one read replica.
+    """
+    replicas_count = self.get_replicas_count()
+
+    if replicas_count >= self.max_capacity:
+      print("maximum capacity reached")
+      return None
+
+    if self.is_modifying():
+      print("is modifying")
+      return None
+
+    return client.create_db_instance(
+      DBClusterIdentifier=self.db_cluster_id,
+      DBInstanceIdentifier="%s-%s" % (self.db_cluster_id, uuid.uuid4().hex[0:8]),
+      DBInstanceClass=self.get_primary_instance_class(),
+      Engine="docdb",
+    )
+
+  def remove_replica(self):
+    """
+    Remove one read replica.
+    """
+    replicas_count = self.get_replicas_count()
+    
+    if replicas_count <= self.min_capacity:
+      print('minimum capacity reached')
+      return None
+    
+    if self.is_modifying():
+      print("is modifying")
+      return None
+
+    db_cluster_members = self.db_clusters.get('DBClusters')[0].get('DBClusterMembers')
+
+    for cluster_member in db_cluster_members:
+      # Remove the first replica instance found
+      if not cluster_member.get('IsClusterWriter'):
+        return client.delete_db_instance(
+          DBInstanceIdentifier=cluster_member.get('DBInstanceIdentifier')
+        )
--- src/index.py
+++ src/index.py
@@ -1,93 +1,25 @@
-# import os
-# import boto3
-# import uuid
+import os
+import json
+import autoscaling
 
-# client = boto3.client('docdb')
+# Environment variables
+min_capacity = int(os.environ.get("min_capacity"))
+max_capacity = int(os.environ.get("max_capacity"))
+cluster_identifier = os.environ.get("cluster_identifier")
 
-# # Environment variables
-# min_capacity = os.environ.get("min_capacity")
-# max_capacity = os.environ.get("max_capacity")
-# cluster_identifier = os.environ.get("cluster_identifier")
+def handler(event, context):
+  sns_message = event.get('Records')[0].get('Sns').get('Message')
+  new_state_value = json.loads(sns_message).get('NewStateValue')
 
-# if min_capacity > max_capacity:
-#   print("min_capacity cannot be greater than max_capacity")
-#   exit()
+  if min_capacity > max_capacity:
+    print("min_capacity cannot be greater than max_capacity")
+    return
 
-# class DocumentDB:
-#   def __init__(self, db_cluster_id):
-#     self.db_cluster_id = db_cluster_id
+  docdb = autoscaling.DocumentDB(cluster_identifier, min_capacity, max_capacity)
 
-#     # Needed to know what is the 'primary instance'
-#     self.db_clusters = client.describe_db_clusters(DBClusterIdentifier=db_cluster_id)
-
-#     # Needed to know the instance class
-#     self.db_instances = client.describe_db_instances(
-#       Filters=[{
-#         'Name': 'db-cluster-id',
-#         'Values': [ db_cluster_id ]
-#       }]
-#     )
-
-#   def is_modifying(self):
-#     db_instances = self.db_instances.get('DBInstances')
-#     for db_instance in db_instances:
-#       if db_instance.get('DBInstanceStatus') != 'available':
-#         return True
-
-#     return False
-
-#   def get_replicas_count(self):
-#     db_instances = self.db_instances.get('DBInstances')
-#     return len(db_instances[1:])
-  
-#   def get_primary_instance_class(self):
-#     db_cluster_members = self.db_clusters.get('DBClusters')[0].get('DBClusterMembers')
-#     db_instances = self.db_instances.get('DBInstances')
-
-#     for cluster_member in db_cluster_members:
-#       is_cluster_writer = cluster_member.get('IsClusterWriter')
-#       if is_cluster_writer:
-#         for db_instance in db_instances:
-#           if cluster_member.get('DBInstanceIdentifier') == db_instance.get('DBInstanceIdentifier'):
-#             return db_instance.get('DBInstanceClass')
-    
-#     # In theory this block never can be executed
-#     return 'db.r5.large'
-
-#   def add_replica(self):
-#     replicas_count = self.get_replicas_count()
-
-#     if replicas_count >= max_capacity:
-#       print("maximum capacity reached")
-#       return None
-
-#     if self.is_modifying():
-#       print("is modifying")
-#       return None
-
-#     return client.create_db_instance(
-#       DBClusterIdentifier=self.db_cluster_id,
-#       DBInstanceIdentifier="%s-%s" % (self.db_cluster_id, uuid.uuid4().hex[0:8]),
-#       DBInstanceClass=self.get_primary_instance_class(),
-#       Engine="docdb",
-#     )
-
-#   def remove_replica(self):
-#     replicas_count = self.get_replicas_count()
-    
-#     if replicas_count <= min_capacity:
-#       print('minimum capacity reached')
-#       return None
-    
-#     if self.is_modifying():
-#       print("is modifying")
-#       return None
-
-#     db_cluster_members = self.db_clusters.get('DBClusters')[0].get('DBClusterMembers')
-
-#     for cluster_member in db_cluster_members:
-#       # Remove the first replica instance found
-#       if not cluster_member.get('IsClusterWriter'):
-#         return client.delete_db_instance(
-#           DBInstanceIdentifier=cluster_member.get('DBInstanceIdentifier')
-#         )
+  if new_state_value == "ALARM":
+    docdb.add_replica()
+    print("Adding replica...")
+  if new_state_value == "OK":
+    docdb.remove_replica()
+    print("Removing replica...")

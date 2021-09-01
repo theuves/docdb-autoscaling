82690ed46a39eec74b96706e144914a3c8b8f7b7
Matheus Alves <theuves@gmail.com>
2021-09-01 00:06:43 -0400

    add more instances when min_capacity is less than replicas count

--- src/index.py
+++ src/index.py
@@ -9,18 +9,35 @@ max_capacity = int(os.environ.get("max_capacity"))
 cluster_identifier = os.environ.get("cluster_identifier")
 
 def handler(event, context):
-  sns_message = event.get('Records')[0].get('Sns').get('Message')
-  new_state_value = json.loads(sns_message).get('NewStateValue')
-
   if min_capacity > max_capacity:
     logging.critical("The 'min_capacity' cannot be greater than 'max_capacity'.")
     return None
 
   docdb = autoscaling.DocumentDB(cluster_identifier, min_capacity, max_capacity)
+  replicas_count = docdb.get_replicas_count()
 
+  # Add more replica instances to meet the minimum capacity
+  if min_capacity > replicas_count:
+    logging.warning("Adding more replica instances to meet the minimum capacity...")
+    missing_replicas = min_capacity - replicas_count
+
+    while missing_replicas > 0:
+      logging.warning("Adding replica...")
+      docdb.add_replica(ignore_status=True)
+      missing_replicas -= 1
+
+    # Ignore the alarm state
+    return None
+
+  sns_message = json.loads(event.get('Records')[0].get('Sns').get('Message'))
+  new_state_value = sns_message.get('NewStateValue')
+
+  # "In alarm" = scale-out
   if new_state_value == "ALARM":
     logging.warning("Adding replica...")
     docdb.add_replica()
+
+  # "OK" = scale-in
   if new_state_value == "OK":
     logging.warning("Removing replica...")
     docdb.remove_replica()
